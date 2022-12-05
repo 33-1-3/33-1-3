159db4ce8ccffd1abecc8c44dcd28b5d3cad0ea9
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-05 16:17:45 +0900

    feat: 전역 스타일과 normalize 스타일 추가
    Co-authored-by: nalsae <nalsae0207@naver.com>

---
+++ src/common/styles/globalStyles.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle/*css*/ `
+  @font-face {
+    font-family: 'LINESeed';
+    src: url('LINESeedKR-Th.woff') format('woff');
+    font-weight: 100;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'LINESeed';
+    src: url('LINESeedKR-Rg.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'LINESeed';
+    src: url('LINESeedKR-Bd.woff') format('woff');
+    font-weight: 700;
+    font-style: normal;
+    font-display: swap;
+  }
+  :root {
+    /* color */
+    --white: #ffffff;
+    --black: #000000;
+    --red: #fe2828;
+    --purple-100: #ece2ff;
+    --purple-900: #6c33e8;
+    --gray-100: #dadada;
+    --gray-200: #979797;
+    --gray-300: #5e5e5e;
+    --gray-400: #505050;
+
+    /* drop shadow */
+    --shadow-Header: "0px 2px 10px 0px rgba(0,0,0,0.3)";
+    --shadow-Item: "2px 2px 3px 0px rgba(0,0,0,0.25)";
+    --shadow-Button-back: "0px 4px 10px 0px rgba(0,0,0,0.3)";
+    --shadow-Modal: "0px 4px 10px 0px rgba(0,0,0,0.5)";
+    --shadow-Vinyl: "4px 4px 10px 0px rgba(0,0,0,0.5)";
+    --shadow-Background: "inset 4px 0px 10px 0px rgba(0,0,0,0.15)";
+    --shadow-catchphrase: "0px 2px 8px 0px rgba(0,0,0,1)";
+
+    /* text size */
+    --text-xs: 12px;
+    --text-sm: 14px;
+    --text-bs: 16px;
+    --text-md: 20px;
+    --text-lg: 28px;
+    --text-xl: 32px;
+    --text-xxl: 44px;
+    --text-xxxl: 56px;
+
+    /* spacing */
+    --space-xs: 8px;
+    --space-sm: 12px;
+    --space-bs: 16px;
+    --space-md: 20px;
+    --space-lg: 24px;
+    --space-xl: 28px;
+    --space-xxl: 32px; 
+  }
+`;
