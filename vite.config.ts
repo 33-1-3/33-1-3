a1a371d12eca839f98c057f4aa035d86ba4f836e
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-06 19:26:06 +0900

    chore: vite-plugin-svgr 설치 및 플러그인 설정
    svgr 컴포넌트를 사용하기 위해 플러그인을 설치하고 환경 구성했습니다.
    타입스크립트에 asset type도 추가했습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
       '@': resolve('./src'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
