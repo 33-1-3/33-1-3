bcbd64d7abd7d61ebae432b2f8baaac39e522b80
nalsae <nalsae0207@naver.com>
2022-12-08 00:49:51 +0900

    TEST: 검색 결과 개수를 표시하는 컴포넌트의 스토리 생성 완료

---
+++ src/common/components/SearchResultText/SearchResultText.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SearchResultText from './SearchResultText';
+
+export default {
+  title: 'common/components/SearchResultText',
+  component: SearchResultText,
+  args: {
+    keyword: '검색어',
+    resultCount: 0,
+  },
+  argTypes: {
+    resultCount: {
+      control: { type: 'range', mix: 0, max: 100, step: 1 },
+    },
+  },
+  parameters: {
+    design: {
+      url: 'https://www.figma.com/file/y5dq4m439YJKRTrKw5ZsZV/33-1%2F3?node-id=684%3A1153&t=IZlZBzSRbsy5uc2f-4',
+    },
+  },
+} as ComponentMeta<typeof SearchResultText>;
+
+const Template: ComponentStory<typeof SearchResultText> = (args) => (
+  <SearchResultText {...args} />
+);
+
+export const Exapmle = Template.bind({});
