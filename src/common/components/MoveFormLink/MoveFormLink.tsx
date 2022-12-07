fdb153442fe4bbe1de78cf62573c6ff1c73b2840
nalsae <nalsae0207@naver.com>
2022-12-07 20:55:01 +0900

    FEAT: 로그인 & 회원가입 링크 컴포넌트 구현 완료

---
+++ src/common/components/MoveFormLink/MoveFormLink.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const INFO_MESSAGES = {
+  signin: '이미 회원이라면?',
+  signup: '아직 회원이 아니라면?',
+};
+const LINK_MESSAGES = {
+  signin: '로그인!',
+  signup: '가입하기!',
+};
+
+export interface MoveFormLinkProps {
+  moveTarget: 'signin' | 'signup';
+}
+
+const LinkWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 6px;
+  color: var(--purple-900);
+  font-size: var(--text-sm);
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: underline;
+  text-underline-position: under;
+`;
+
+function MoveFormLink({ moveTarget }: MoveFormLinkProps) {
+  return (
+    <LinkWrapper>
+      <span>{INFO_MESSAGES[moveTarget]}</span>
+      <StyledLink to={`/${moveTarget}`}>{LINK_MESSAGES[moveTarget]}</StyledLink>
+    </LinkWrapper>
+  );
+}
+
+export default MoveFormLink;
