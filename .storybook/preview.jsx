5c02f4b278e788aa5d9b8827d83d8a94875a095b
nalsae <nalsae0207@naver.com>
2022-12-07 17:26:19 +0900

    CHORE: Story Book Figma 애드온 추가
    - 접근성 애드온, 디자인 애드온, 액션 애드온 추가 완료

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,6 @@
 import { GlobalStyle } from '../src/common/styles/globalStyle';
 import { NormalizeStyle } from '../src/common/styles/normalizeStyle';
+import ko from 'axe-core/locales/ko.json';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -8,6 +9,9 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  a11y: {
+    config: { locale: ko },
   },
 };
 
