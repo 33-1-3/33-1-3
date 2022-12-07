98f163b6f2d2d25494c5421c5139e203b2eac36e
nalsae <nalsae0207@naver.com>
2022-12-07 14:34:55 +0900

    REFACTOR: defaultProps 추가 및 확장성 관련 수정

--- src/common/components/SquareButton/SquareButton.tsx
+++ src/common/components/SquareButton/SquareButton.tsx
@@ -1,14 +1,16 @@
 import styled from 'styled-components';
 
+const paddingValue = { small: '10px 16px', large: '12px 24px' };
+
 export interface SqaureButtonProps {
-  fontSize: string;
-  isBig: boolean;
+  fontSize: number;
+  size: 'small' | 'large';
   isFilled: boolean;
   children: string;
 }
 
 const SqaureButton = styled.button<SqaureButtonProps>`
-  padding: ${({ isBig }) => (isBig ? '12px 24px' : '10px 16px')};
+  padding: ${({ size }) => paddingValue[size]};
   background-color: ${({ isFilled }) =>
     isFilled ? 'var(--purple-900)' : 'var(--white)'};
   border: ${({ isFilled }) =>
@@ -17,8 +19,15 @@ const SqaureButton = styled.button<SqaureButtonProps>`
   font-family: 'LINESeed';
   font-weight: 700;
   text-align: center;
-  font-size: ${({ fontSize }) => fontSize};
+  font-size: ${({ fontSize }) => fontSize}px;
   color: ${({ isFilled }) => (isFilled ? 'var(--white)' : 'var(--purple-900)')};
 `;
+
+SqaureButton.defaultProps = {
+  fontSize: 20,
+  size: 'small',
+  isFilled: true,
+  children: '버튼',
+};
 
 export default SqaureButton;
