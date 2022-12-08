5ff2e99414293ff5ed0b4673e3f7a881fc014a91
chaerin-dev <chaerin.dev@gmail.com>
2022-12-08 16:47:06 +0900

    FEAT: 접근성을 위한 srOnly 스타일 추가

--- src/common/styles/globalStyle.ts
+++ src/common/styles/globalStyle.ts
@@ -71,4 +71,16 @@ export const GlobalStyle = createGlobalStyle/*css*/ `
     font-family: 'LINESeed';
     font-weight: 700;
   }
+  .srOnly {
+    overflow: hidden;
+    position: absolute;
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+  }
 `;
