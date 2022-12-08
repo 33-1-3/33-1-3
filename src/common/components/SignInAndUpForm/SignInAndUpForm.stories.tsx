70d9dfd6ca727689518e4ef2e2272fe909982a5d
chaerin-dev <chaerin.dev@gmail.com>
2022-12-08 16:48:14 +0900

    FEAT: SignInAndUpInput 컴포넌트 테스트코드 작성

---
+++ src/common/components/SignInAndUpForm/SignInAndUpForm.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import SignInAndUpForm from './SignInAndUpForm';
+
+export default {
+  title: 'common/components/SignInAndUpForm',
+  component: SignInAndUpForm,
+} as ComponentMeta<typeof SignInAndUpForm>;
+
+const Template: ComponentStory<typeof SignInAndUpForm> = (args) => (
+  <SignInAndUpForm {...args} />
+);
+
+export const SigninForm = Template.bind({});
+SigninForm.args = { option: 'signin' };
+
+export const SignupForm = Template.bind({});
+SignupForm.args = { option: 'signup' };
