560e76c8e6c9a0ca29a71e42e361afe72fe34be0
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-09 18:24:57 +0900

    FEAT: Storybook 테스트 코드 작성

---
+++ src/pages/Item/components/SelectCollectionForm/SelectCollectionForm.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import SelectCollectionForm from '../SelectCollectionForm/SelectCollectionForm';
+
+export default {
+  title: 'pages/Item/SelectCollectionForm',
+  component: SelectCollectionForm,
+  parameters: {
+    design: {
+      url: 'https://www.figma.com/file/y5dq4m439YJKRTrKw5ZsZV/33-1%2F3?node-id=181%3A2817&t=2EzCr9k6QbQSABYL-4',
+    },
+  },
+} as ComponentMeta<typeof SelectCollectionForm>;
+
+const Template: ComponentStory<typeof SelectCollectionForm> = (args) => (
+  <SelectCollectionForm {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  mockCollectionList: [],
+};
+
+export const List = Template.bind({});
+List.args = {
+  mockCollectionList: [
+    { title: '소장 중 💼', isChecked: false },
+    { title: '갖고 싶다... 🤤', isChecked: true },
+    { title: '❤K-POP❤', isChecked: false },
+    {
+      title: '엄청엄청긴타이트으으응으ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ일때',
+      isChecked: false,
+    },
+  ],
+};
