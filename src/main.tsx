18121b1dbe4f4ee7428efc6170b01bfe219aea5a
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-10 15:47:51 +0900

    REFACTOR: css파일 제거 및 불필요한 코드 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
-import '@/index.css';
 import { GlobalStyle } from '@/common/styles/globalStyle';
 import { NormalizeStyle } from '@/common/styles/normalizeStyle';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <App />
