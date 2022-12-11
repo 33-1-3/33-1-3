c0df393af895fb8c8cc79a4c73a57791b6f47b65
chaerin-dev <chaerin.dev@gmail.com>
2022-12-11 16:15:51 +0900

    FEAT: PageTitle 컴포넌트 구현

---
+++ src/common/components/PageTitle/PageTitle.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const H2 = styled.h2`
+  font-size: 48px;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+`;
+
+export interface PageTitleProps {
+  children: string;
+}
+
+const PageTitle = ({ children, ...props }: PageTitleProps) => {
+  return <H2 {...props}>{children}</H2>;
+};
+
+export default PageTitle;
