8a588dc69ab4d6978cd67d886ba7c2a67b4fe131
nalsae <nalsae0207@naver.com>
2022-12-12 13:18:11 +0900

    STYLE: 리스트 prop을 나의 콜렉션으로 변경

--- src/common/components/DetailInfo/DetailInfo.stories.tsx
+++ src/common/components/DetailInfo/DetailInfo.stories.tsx
@@ -4,9 +4,6 @@ import DetailInfo from './DetailInfo';
 export default {
   title: 'common/components/DetailInfo',
   component: DetailInfo,
-  args: {
-    isValid: true,
-  },
   parameters: {
     design: {
       url: 'https://www.figma.com/file/y5dq4m439YJKRTrKw5ZsZV/33-1%2F3?node-id=377%3A2244&t=ri42uDzp7Wjgw7vi-4',
@@ -21,29 +18,17 @@ const Template: ComponentStory<typeof DetailInfo> = (args) => (
 export const Released = Template.bind({});
 Released.args = {
   infoName: 'Released',
-  infoContent: '2014',
+  infoContent: 'Aug 2014',
 };
 
 export const Genre = Template.bind({});
 Genre.args = {
   infoName: 'Genre',
-  infoContent: ['Pop', 'Folk, World, & Country'],
+  infoContent: 'Pop, Folk, World, & Country',
 };
 
 export const Style = Template.bind({});
-Style.args = {
-  infoName: 'Style',
-  infoContent: ['Folk', 'Ballad', 'K-pop'],
-};
-
-export const Country = Template.bind({});
-Country.args = {
-  infoName: 'Country',
-  infoContent: 'South Korea',
-};
-
-export const Label = Template.bind({});
-Label.args = {
-  infoName: 'Label',
-  infoContent: ['Loen Entertainment'],
+Genre.args = {
+  infoName: 'Genre',
+  infoContent: 'Pop, Folk, World, & Country',
 };
