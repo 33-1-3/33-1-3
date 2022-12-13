6b56df564d522c9ba74f5702b553d58b56cd13e9
chjy0202 <chjy0202@naver.com>
2022-12-14 02:02:40 +0900

    FEAT: 페이지 이동 시 스크롤 초기화하는 컴포넌트 구현

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import { GlobalStyle } from '@/common/styles/globalStyle';
 import { NormalizeStyle } from '@/common/styles/normalizeStyle';
+import { ScrollToTop } from '@/common/components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Router>
         <NormalizeStyle />
         <GlobalStyle />
+        <ScrollToTop />
         <App />
       </Router>
     </RecoilRoot>
