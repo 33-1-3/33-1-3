ec82bd96fc90b249df2de93173b7d69e64e12356
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-19 00:39:41 +0900

    FEAT: 로그인, 회원가입 에러 처리
    - 이메일 중복
    - 이메일 미인증
    - 이메일, 비밀번호 불일치

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -5,6 +5,8 @@ import {
   SignInAndUpForm,
   WaveFooter,
 } from '@/common/components';
+import axios from 'axios';
+import { useState } from 'react';
 
 const HeaderLogo = styled(LogoLink)`
   margin-bottom: 24px;
@@ -20,13 +22,49 @@ const FormContainer = styled.div`
   margin-top: 15vh;
 `;
 
-export default function Signin() {
+const AlertMsg = styled.div`
+  margin-top: var(--space-bs);
+  font-size: var(--text-sm);
+  font-weight: 700;
+  text-align: center;
+`;
+
+const url = `http://localhost:3313/signup`;
+
+export default function Signup() {
+  const [checkEmail, setCheckEmail] = useState();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const nickname = e.target[1].value;
+    const password = e.target[2].value;
+    const {
+      data: { state },
+    } = await axios.post(url, {
+      email,
+      nickname,
+      password,
+    });
+
+    setCheckEmail(state);
+  };
+
   return (
     <>
       <FormContainer>
         <HeaderLogo height="72px" width="132px" />
-        <SignInAndUpForm option="signup" />
+        <SignInAndUpForm submitHandler={submitHandler} option="signup" />
         <MoveLink moveTarget="signin" />
+        {checkEmail === 'duplicate' && (
+          <AlertMsg>이미 등록된 이메일 입니다.</AlertMsg>
+        )}
+        {checkEmail == 'success' && (
+          <AlertMsg>
+            등록한 이메일로 인증 메일이 발송되었습니다.
+            <br />
+            이메일 인증을 완료해주세요.
+          </AlertMsg>
+        )}
       </FormContainer>
       <WaveFooter />
     </>
