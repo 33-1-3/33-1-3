f1007507615baddf9d7e147598532c90036041f4
GuRaBang <qkrdbsgk00@gmail.com>
2022-12-21 02:41:55 +0900

    FEAT: masters, releases api 요청에 대한 응답 데이터 가공 함수 구현
    - 기존에는 Item페이지 렌더링 시 tracklist만 masters, releases api에서
      필터링 방식
    - My Item 페이지 렌더링과 Item 페이지 렌더링 방식의 일관성을 위해 렌더링
      시 필요한 데이터를 releases, masters api에서 가져오도록 수정
    - masters, releases api 각각의 응답 데이터를 가공하는 함수를 구현
    - 필요한 타입 지정

--- src/utils/functions/processResult.ts
+++ src/utils/functions/processResult.ts
@@ -1,4 +1,11 @@
-import { RawResult, ProcessedResult } from '@/types/data';
+import {
+  RawResult,
+  ReleaseRawResult,
+  MasterRawResult,
+  RawTracklist,
+  ProcessedResult,
+  ProcessedResourceUrlResult,
+} from '@/types/data';
 
 const validator = (data: string | Array<string>) => {
   if (typeof data === 'string') {
@@ -10,10 +17,16 @@ const validator = (data: string | Array<string>) => {
   return false;
 };
 
-const processResult = (result: RawResult[]): ProcessedResult[] =>
+const getId = (resource_url: string): string => {
+  const arr = resource_url.split('/');
+  const { length } = arr;
+  return arr[length - 2] + arr[length - 1];
+};
+
+// Search API 결과 처리
+const processSearchResult = (result: RawResult[]): ProcessedResult[] =>
   result.map(
     ({
-      id,
       country,
       cover_image,
       genre,
@@ -26,7 +39,7 @@ const processResult = (result: RawResult[]): ProcessedResult[] =>
       const [artist, albumTitle] = title.split(' - ');
 
       return {
-        id: id,
+        id: getId(resource_url),
         titleInfo: { title: albumTitle, artist },
         detailInfo: [
           {
@@ -61,4 +74,138 @@ const processResult = (result: RawResult[]): ProcessedResult[] =>
     }
   );
 
-export default processResult;
+const tracklistVaildator = (tracklist: RawTracklist[]) =>
+  tracklist.length !== 0 && Array.isArray(tracklist);
+
+// Release API 결과 처리
+const processReleaseResult = ({
+  country,
+  title,
+  artists,
+  images,
+  genres,
+  labels: _labels,
+  resource_url,
+  styles,
+  year: _year,
+  released: _released,
+  tracklist: _tracklist,
+}: ReleaseRawResult): ProcessedResourceUrlResult => {
+  const artist = artists[0].name;
+  const imgUrl = images[0].resource_url;
+  const labels = _labels.map(({ name }) => name);
+  const tracklist = _tracklist.map(({ position, type_, title, duration }) => ({
+    position,
+    type_,
+    title,
+    duration,
+  }));
+  const released = _released === undefined ? '' : _released;
+  const year = released === '' ? _year ?? '' : released;
+
+  return {
+    id: getId(resource_url),
+    titleInfo: { title, artist },
+    detailInfo: [
+      {
+        infoName: 'Released',
+        infoContent: year,
+        isValid: validator(year),
+      },
+      {
+        infoName: 'Genre',
+        infoContent: genres,
+        isValid: validator(genres),
+      },
+      {
+        infoName: 'Style',
+        infoContent: styles,
+        isValid: validator(styles),
+      },
+      {
+        infoName: 'Country',
+        infoContent: country,
+        isValid: validator(country),
+      },
+      {
+        infoName: 'Label',
+        infoContent: labels,
+        isValid: validator(labels),
+      },
+    ],
+    imgUrl,
+    resourceUrl: resource_url,
+    tracklist: {
+      infoName: 'Tracklist',
+      infoContent: tracklist,
+      isValid: tracklistVaildator(tracklist),
+    },
+  };
+};
+
+// Master API 결과 처리
+const processMasterResult = ({
+  country: _country,
+  title,
+  artists,
+  images,
+  genres,
+  labels: _labels,
+  resource_url,
+  styles,
+  year: _year,
+  tracklist: _tracklist,
+}: MasterRawResult): ProcessedResourceUrlResult => {
+  const country = _country ?? '';
+  const artist = artists[0].name;
+  const imgUrl = images[0].resource_url;
+  const year = _year ?? '';
+  const labels = _labels?.map(({ name }) => name) ?? [];
+  const tracklist = _tracklist.map(({ position, type_, title, duration }) => ({
+    position,
+    type_,
+    title,
+    duration,
+  }));
+
+  return {
+    id: getId(resource_url),
+    titleInfo: { title, artist },
+    detailInfo: [
+      {
+        infoName: 'Released',
+        infoContent: year,
+        isValid: validator(year),
+      },
+      {
+        infoName: 'Genre',
+        infoContent: genres,
+        isValid: validator(genres),
+      },
+      {
+        infoName: 'Style',
+        infoContent: styles,
+        isValid: validator(styles),
+      },
+      {
+        infoName: 'Country',
+        infoContent: country,
+        isValid: validator(country),
+      },
+      {
+        infoName: 'Label',
+        infoContent: labels,
+        isValid: validator(labels),
+      },
+    ],
+    imgUrl,
+    resourceUrl: resource_url,
+    tracklist: {
+      infoName: 'Tracklist',
+      infoContent: tracklist,
+      isValid: tracklistVaildator(tracklist),
+    },
+  };
+};
+
+export { processSearchResult, processReleaseResult, processMasterResult };
