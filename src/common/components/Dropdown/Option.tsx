6833b64934d1a55e0c4bc1d129e1c3a1e96b59f4
nalsae <nalsae0207@naver.com>
2022-12-23 02:53:04 +0900

    FEAT: 드롭다운 컴포넌트 애니메이션 구현

--- src/common/components/Dropdown/Option.tsx
+++ src/common/components/Dropdown/Option.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 export interface OptionProps {
   value: string;
   children: string;
 }
 
-const Li = styled.li`
+const Li = styled(motion.li)`
   display: flex;
   justify-content: center;
   align-items: center;
   list-style: none;
+  border-radius: 2px;
 
   &:not(:last-child) {
     border-bottom: 1px solid var(--gray-200);
@@ -26,6 +28,14 @@ const Option = ({ value, children, ...props }: OptionProps) => {
       role="option"
       style={{ width: '100%', height: '100%' }}
       value={value}
+      whileHover={{
+        backgroundColor: 'var(--purple-100)',
+        color: 'var(--black)',
+      }}
+      transition={{
+        duration: 0.3,
+        ease: 'easeOut',
+      }}
       {...props}
     >
       {children}
