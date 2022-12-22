81e1c5d5a68cb18ea18d7ae67818f5cfb1a11fce
chaerin-dev <chaerin.dev@gmail.com>
2022-12-22 17:27:49 +0900

    REFACTOR: 로그인 여부 확인 방식 변경
    각 컴포넌트에서 로그인 여부를 확인하던 것을 전역 상태에서 관리하는 방식으로 바꿈
    
    Co-Authored-By: GuRaBang <87111950+GuRaBang@users.noreply.github.com>
    Co-Authored-By: Hyunjung Choi <72859045+chjy0202@users.noreply.github.com>
    Co-Authored-By: nalsae <101828759+nalsae@users.noreply.github.com>

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -4,10 +4,14 @@ import {
   MoveLink,
   SignInAndUpForm,
   WaveFooter,
+  Alert,
 } from '@/common/components';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect, useLayoutEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { loginState } from '@/recoil/globalState';
+// import { authState } from '@/recoil/globalState';
 
 const HeaderLogo = styled(LogoLink)`
   margin-bottom: 24px;
@@ -23,41 +27,61 @@ const FormContainer = styled.div`
   margin-top: 20vh;
 `;
 
-const authRequestUrl = `${import.meta.env.VITE_DB_SERVER}auth`;
+// const authRequestUrl = `${import.meta.env.VITE_DB_SERVER}auth`;
 const signinRequestUrl = `${import.meta.env.VITE_DB_SERVER}signin`;
+// import { userState } from '../../recoil/globalState';
 
 export default function Signin() {
+  const [isLogIn, setIsLogIn] = useRecoilState(loginState);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  // const [userId, setUserId] = useRecoilState(userState);
+  // const [auth, setAuth] = useRecoilState(authState);
   const [checkEmail, setCheckEmail] = useState('');
   const navigate = useNavigate();
 
-  useLayoutEffect(() => {
-    async function auth() {
-      const {
-        data: { isLogin },
-      } = await axios.get(authRequestUrl, {
-        withCredentials: true,
-      });
-
-      isLogin && navigate('/');
+  useEffect(() => {
+    if (showAlert) {
+      setTimeout(() => setShowAlert(false), 4000);
     }
-    auth();
+  }, [showAlert]);
+
+  useLayoutEffect(() => {
+    //   async function auth() {
+    //     const {
+    //       data: { isLogin },
+    //     } = await axios.get(authRequestUrl, {
+    //       withCredentials: true,
+    //     });
+
+    //     isLogin && navigate('/');
+    //   }
+    //   auth();
+    // TODO: 오류?
+    isLogIn && navigate('/');
   }, []);
 
-  useEffect(() => {
-    checkEmail === 'needAuth' &&
-      alert('인증이 되지 않은 이메일입니다. 이메일 인증을 완료해주세요.');
+  // useEffect(() => {
+  //   checkEmail === 'needAuth' &&
+  //     alert('인증이 되지 않은 이메일입니다. 이메일 인증을 완료해주세요.');
 
-    checkEmail === 'notExist' &&
-      alert('이메일 혹은 비밀번호가 일치하지 않습니다.');
+  //   checkEmail === 'notExist' &&
+  //     alert('이메일 혹은 비밀번호가 일치하지 않습니다.');
 
-    setCheckEmail('');
-  }, [checkEmail]);
+  //   setCheckEmail('');
+  // }, [checkEmail]);
 
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const email = (e.currentTarget[0] as HTMLInputElement).value;
     const password = (e.currentTarget[1] as HTMLInputElement).value;
+
+    (e.target as HTMLFormElement).reset();
+    (
+      e.currentTarget[e.currentTarget.length - 1] as HTMLButtonElement
+    ).disabled = true;
+    (e.currentTarget[0] as HTMLInputElement).focus();
+
     const {
       data: { userId, state },
     } = await axios.post(
@@ -65,12 +89,27 @@ export default function Signin() {
       { email, password },
       { withCredentials: true }
     );
-    if (userId) navigate('/');
-    else setCheckEmail(state);
+
+    if (userId) {
+      navigate('/');
+    } else {
+      setCheckEmail(state);
+      setShowAlert(true);
+    }
   };
 
   return (
     <>
+      {checkEmail === 'needAuth' && showAlert && (
+        <Alert width="400px" height="80px" type="top">
+          인증이 되지 않은 이메일입니다. 이메일 인증을 완료해주세요.
+        </Alert>
+      )}
+      {checkEmail === 'notExist' && showAlert && (
+        <Alert width="400px" height="80px" type="top">
+          이메일 혹은 비밀번호가 일치하지 않습니다.
+        </Alert>
+      )}
       <FormContainer>
         <HeaderLogo height="72px" width="132px" />
         <SignInAndUpForm submitHandler={submitHandler} option="signin" />
