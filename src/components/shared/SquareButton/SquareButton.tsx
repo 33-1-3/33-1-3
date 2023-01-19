c97ac12a3e71fd1df41d344d6cc7eceea0d78a2a
nalsae <nalsae0207@naver.com>
2023-01-20 00:50:41 +0900

    REFACTOR: SquareButton 컴포넌트 리팩토링 완료
    - 객체 불변성 지향
    - mixin 사용하여 가독성 향상
    - width 속성 추가

--- src/components/shared/SquareButton/SquareButton.tsx
+++ src/components/shared/SquareButton/SquareButton.tsx
@@ -1,6 +1,4 @@
-import styled from 'styled-components';
-
-const PADDING_VALUE = { small: '10px 16px', large: '12px 24px' };
+import styled, { css } from 'styled-components';
 
 export interface SquareButtonProps {
   fontSize: number;
@@ -9,32 +7,47 @@ export interface SquareButtonProps {
   children: string;
 }
 
-const SquareButton = styled.button<SquareButtonProps>`
-  padding: ${({ size }) => PADDING_VALUE[size]};
-  background-color: ${({ isFilled }) =>
-    isFilled ? 'var(--purple-900)' : 'var(--white)'};
-  border: ${({ isFilled }) =>
-    isFilled ? 'none' : '1px solid var(--purple-900)'};
-  border-radius: 6px;
-  font-weight: 700;
-  text-align: center;
-  font-size: ${({ fontSize }) => fontSize}px;
-  color: ${({ isFilled }) => (isFilled ? 'var(--white)' : 'var(--purple-900)')};
-  cursor: pointer;
-
-  &:disabled {
+const PADDING_VALUE = Object.freeze({ small: '10px 16px', large: '12px 24px' });
+const BUTTON_STYLE = Object.freeze({
+  isFilled: css`
+    background-color: var(--purple-900);
+    border: none;
+    color: var(--white);
+  `,
+  isNotFilled: css`
+    background-color: var(--white);
+    border: 1px solid var(--purple-900);
+    color: var(--purple-900);
+  `,
+  disabled: css`
     background-color: var(--gray-100);
     border: none;
     color: var(--gray-300);
+  `,
+});
+
+const SquareButton = styled.button<SquareButtonProps>`
+  ${({ isFilled }) =>
+    isFilled ? BUTTON_STYLE.isFilled : BUTTON_STYLE.isNotFilled}
+  width: fit-content;
+  padding: ${({ size }) => PADDING_VALUE[size]};
+  border-radius: 6px;
+  font-size: ${({ fontSize }) => fontSize}px;
+  font-weight: 700;
+  text-align: center;
+  cursor: pointer;
+
+  &:disabled {
+    ${BUTTON_STYLE.disabled}
     cursor: not-allowed;
   }
 `;
 
-SquareButton.defaultProps = {
+SquareButton.defaultProps = Object.freeze({
   fontSize: 20,
   size: 'small',
   isFilled: true,
   children: '버튼',
-};
+});
 
 export default SquareButton;
