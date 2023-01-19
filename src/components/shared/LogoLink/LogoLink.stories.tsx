ac8b2cc54f48c5de72b4f13d540729e2097f1522
nalsae <nalsae0207@naver.com>
2023-01-19 22:55:07 +0900

    TEST: LogoLink 컴포넌트 StoryBook 재작성
    - 기존 small, large로 구분하는 Story를 사용처에 따라 Header,
      SignInAndUp, NotFound로 변경
    - docs 탭에 컴포넌트, props 설명 추가

--- src/components/shared/LogoLink/LogoLink.stories.tsx
+++ src/components/shared/LogoLink/LogoLink.stories.tsx
@@ -2,11 +2,29 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import LogoLink from './LogoLink';
 
 export default {
-  title: 'common/components/LogoLink',
+  title: 'components/shared/LogoLink',
   component: LogoLink,
   parameters: {
+    docs: {
+      description: {
+        component:
+          '33 1/3의 로고를 나타내며, 클릭 시 홈페이지로 이동하는 컴포넌트입니다.',
+      },
+    },
     design: {
       url: 'https://www.figma.com/file/y5dq4m439YJKRTrKw5ZsZV/33-1%2F3?node-id=129%3A28&t=ri42uDzp7Wjgw7vi-4',
+    },
+  },
+  argTypes: {
+    width: {
+      description: '로고의 너비 지정',
+      control: 'select',
+      options: ['74px', '100px', '132px'],
+    },
+    height: {
+      description: '로고의 높이 지정',
+      control: 'select',
+      options: ['40px', '54px', '72px'],
     },
   },
 } as ComponentMeta<typeof LogoLink>;
@@ -15,14 +33,20 @@ const Template: ComponentStory<typeof LogoLink> = (args) => (
   <LogoLink {...args} />
 );
 
-export const Large = Template.bind({});
-Large.args = {
+export const Header = Template.bind({});
+Header.args = {
+  width: '74px',
+  height: '40px',
+};
+
+export const SignInAndUp = Template.bind({});
+SignInAndUp.args = {
   width: '132px',
   height: '72px',
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  width: '74px',
-  height: '40px',
+export const NotFound = Template.bind({});
+NotFound.args = {
+  width: '100px',
+  height: '54px',
 };
