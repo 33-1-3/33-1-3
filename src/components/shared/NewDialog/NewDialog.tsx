bbef7d8e70c429598f69218e484318d9663275c1
nalsae <nalsae0207@naver.com>
2023-03-03 22:36:53 +0900

    REFACTOR: 리팩토링된 SquareButton 컴포넌트 적용

--- src/components/shared/NewDialog/NewDialog.tsx
+++ src/components/shared/NewDialog/NewDialog.tsx
@@ -113,13 +113,19 @@ function NewDialog({
                 <NodeContent>{children}</NodeContent>
               )}
               <Buttons>
-                <SquareButton type="submit" fontSize={18} size={'large'}>
+                <SquareButton
+                  type="submit"
+                  $width={80}
+                  $height={40}
+                  $fontSize={18}
+                >
                   확인
                 </SquareButton>
                 <SquareButton
                   type="button"
-                  fontSize={18}
-                  size={'large'}
+                  $width={80}
+                  $height={40}
+                  $fontSize={18}
                   isFilled={false}
                   onClick={handleClose}
                 >
