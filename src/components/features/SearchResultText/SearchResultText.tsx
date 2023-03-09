927f43ebe671156f30a3e15506ee00100a8a26b4
chaerin.dev <chaerin.dev@gmail.com>
2023-03-09 19:38:18 +0900

    Refactor: Rename variable

--- src/components/features/SearchResultText/SearchResultText.tsx
+++ src/components/features/SearchResultText/SearchResultText.tsx
@@ -9,18 +9,18 @@ export interface SearchResultTextProps {
 
 function SearchResultText({ resultCount, searchWord }: SearchResultTextProps) {
   const [searchParams] = useSearchParams();
-  const query = searchWord || searchParams.get('query') || '';
-  const formattedQuery =
-    '"' + (query.length > 20 ? query.substring(0, 20) + '… ' : query) + '"';
+  const queryString = searchWord || searchParams.get('query') || '';
+  const formattedQueryString =
+    '"' + (queryString.length > 20 ? queryString.substring(0, 20) + '… ' : queryString) + '"';
 
   return useMemo(
     () => (
       <SearchResultTextWrapper>
         {/* TODO: 임시방편으로 이렇게 구현해두긴 했지만..
         빈 문자열이 검색되었을 경우 허수 검색결과가 많아서 아예 다른 뷰를 보여주는 게 나을듯.. */}
-        {query ? (
+        {queryString ? (
           <>
-            <PurpleText>{formattedQuery}</PurpleText>
+            <PurpleText>{formattedQueryString}</PurpleText>
             <NormalText>
               &nbsp;&nbsp;검색 결과&nbsp;&nbsp;{resultCount} 건
             </NormalText>
@@ -30,7 +30,7 @@ function SearchResultText({ resultCount, searchWord }: SearchResultTextProps) {
         )}
       </SearchResultTextWrapper>
     ),
-    [resultCount, query]
+    [resultCount, queryString]
   );
 }
 
