df98450bfbc30408ea22c308f8f37715fe6a70b9
nalsae <nalsae0207@naver.com>
2023-03-11 22:47:27 +0900

    REFACOTR: 새로 정의한 widthHeight interface로 prop 타입 수정

--- src/components/shared/LogoLink/LogoLink.tsx
+++ src/components/shared/LogoLink/LogoLink.tsx
@@ -1,17 +1,13 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
-
-export interface LogoProps {
-  width: string | number;
-  height: string | number;
-}
+import { widthHeight } from '@/types/style';
 
 const LOGO_PATH = '/assets/logo.svg';
 
-const LogoLink = ({ width, height }: LogoProps) => {
+const LogoLink = ({ $width, $height }: widthHeight) => {
   return (
     <Link to="/" aria-label="홈페이지로 이동">
-      <img src={LOGO_PATH} alt="" width={width} height={height} />
+      <img src={LOGO_PATH} alt="" width={$width} height={$height} />
     </Link>
   );
 };
