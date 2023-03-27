61b45d4ddbbd39dad0cc85debd29282ac561becc
nalsae <nalsae0207@naver.com>
2023-03-28 02:12:16 +0900

    REFACTORING: 검색어 미입력 시 렌더링할 임시 코드 삭제

--- src/components/features/SearchResultText/SearchResultText.tsx
+++ src/components/features/SearchResultText/SearchResultText.tsx
@@ -11,23 +11,21 @@ function SearchResultText({ resultCount, searchWord }: SearchResultTextProps) {
   const [searchParams] = useSearchParams();
   const queryString = searchWord || searchParams.get('query') || '';
   const formattedQueryString =
-    '"' + (queryString.length > 20 ? queryString.substring(0, 20) + '… ' : queryString) + '"';
+    '"' +
+    (queryString.length > 20
+      ? queryString.substring(0, 20) + '… '
+      : queryString) +
+    '"';
 
   return useMemo(
     () => (
       <SearchResultTextWrapper>
-        {/* TODO: 임시방편으로 이렇게 구현해두긴 했지만..
-        빈 문자열이 검색되었을 경우 허수 검색결과가 많아서 아예 다른 뷰를 보여주는 게 나을듯.. */}
-        {queryString ? (
-          <>
-            <PurpleText>{formattedQueryString}</PurpleText>
-            <NormalText>
-              &nbsp;&nbsp;검색 결과&nbsp;&nbsp;{resultCount} 건
-            </NormalText>
-          </>
-        ) : (
-          <NormalText>전체 LP&nbsp;&nbsp;{resultCount} 건</NormalText>
-        )}
+        <>
+          <PurpleText>{formattedQueryString}</PurpleText>
+          <NormalText>
+            &nbsp;&nbsp;검색 결과&nbsp;&nbsp;{resultCount} 건
+          </NormalText>
+        </>
       </SearchResultTextWrapper>
     ),
     [resultCount, queryString]
